aaa8ed41d4e0bbf6895e61ec74816a1469ba7632
pawbu <pawbu@users.noreply.github.com>
2022-12-01 10:49:45 +0100

    Day 1: 2/2

--- tests/test_code.py
+++ tests/test_code.py
@@ -13,3 +13,19 @@ def test_day1_1():
                 calories += int(line.strip())
         print(f'Max calories: {current_max}')
 
+
+def test_day1_2():
+    with open(f'{INPUT_DIR}/day1/input.txt', encoding='utf-8') as f:
+        lines = [line.strip() for line in f]
+
+    all_elves = []
+    calories = 0
+    for line in lines:
+        if line == '':
+            all_elves.append(calories)
+            calories = 0
+        else:
+            calories += int(line)
+    all_elves.sort()
+    top_three = sum(all_elves[-3:])
+    print(f'Top three: {top_three}')
